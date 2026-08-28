6dbb66c95cf4b49f48bb2a1cfa2ffcea09f980ad
Junhyunny <kang3966@naver.com>
2026-08-28 23:11:53 +0900

    chore: initialize iOS app project

---
+++ telecom/telecom/telecomApp.swift
@@ -0,0 +1,17 @@
+//
+//  telecomApp.swift
+//  telecom
+//
+//  Created by 강준현 on 8/27/26.
+//
+
+import SwiftUI
+
+@main
+struct telecomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
